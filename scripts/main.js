31f2a930e3f349da1037eb0fffe9fd656f37784e
Shannaflenda <137124343+Shannaflenda@users.noreply.github.com>
2025-08-08 17:21:42 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
 Object.keys(Planets).forEach(p =>{if (Planets[p]!=null){Planets[p].alwaysUnlocked = true}})
 Object.keys(UnitTypes).forEach(p =>{if (UnitTypes[p]!=null){UnitTypes[p].hidden = false}})
-Object.keys(Blocks).forEach(p =>{if (Blocks[p]!=null){Blocks[p].buildVisibility = BuildVisibility.shown, Blocks[p].buildVisibility = BuildVisibility.shown}})
+Object.keys(Blocks).forEach(p =>{if (Blocks[p]!=null){Blocks[p].buildVisibility = BuildVisibility.shown}})
+	
+Events.on(UnitDamageEvent, event =>{
+	const unit = event.unit;
+	if (unit == null || unit.type.name != "elfly-moony") return;
+	if (unit.health <= unit.maxHealth / 2 && !unit.hasEffect(StatusEffects.overclock)){
+		unit.apply(StatusEffects.overclock, 216000);
+	}
+})
