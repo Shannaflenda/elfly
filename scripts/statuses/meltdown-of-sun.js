ae67110e9154a9d840d55d90d08cd46913262e62
Shannaflenda <137124343+Shannaflenda@users.noreply.github.com>
2025-10-11 21:55:16 +0800

    Add files via upload

--- scripts/statuses/meltdown-of-sun.js
+++ scripts/statuses/meltdown-of-sun.js
@@ -1,16 +1,42 @@
 const tool = require("elfly/libs/tool");
 
-Events.on(UnitDamageEvent, event =>{
+Events.on(UnitDamageEvent, e =>{
 	
-	const unit = event.unit;
-	const bullet = event.bullet;
+	const unit = e.unit;
+	const bullet = e.bullet;
+	const owner = e.bullet.owner;
 	
 	const meltdownOfSun = Vars.content.statusEffect("elfly-meltdown-of-sun");
 	
 	if (unit == null || !unit.hasEffect(meltdownOfSun)) return;
 	
-	var unitRealHealth = tool.getUnitRealMaxHealth(unit);
-	//unit.damagePierce(unitRealHealth * Math.ceil(unit.getDuration(meltdownOfSun) / 60) * 0.01);
-	unit.health -= unit.maxHealth * Math.ceil(unit.getDuration(meltdownOfSun) / 60) * 0.01;
+	var damage = unit.maxHealth * Math.ceil(unit.getDuration(meltdownOfSun) / 60) * 0.01;
+	
+	unit.health -= damage;
 	unit.unapply(meltdownOfSun);
+	
+	if (owner.block != null && owner.block.name == "elfly-soul-distorter") {
+		owner.health += Math.min(damage, owner.maxHealth - owner.health);
+	}
+});
+
+Events.on(BuildDamageEvent, e =>{
+	
+	const building = e.build;
+	const bullet = e.source.type;
+	const owner = e.source.owner;
+	
+	const meltdownOfSun = Vars.content.statusEffect("elfly-meltdown-of-sun");
+		
+	if (building == null || bullet.status != meltdownOfSun) return;
+	
+	var damage = building.maxHealth * Math.ceil(bullet.statusDuration / 60) * 0.01;
+	
+	building.health -= damage;
+	
+	if (owner.block != null && owner.block.name == "elfly-soul-distorter") {
+		owner.health += Math.min(damage, owner.maxHealth - owner.health);
+	}
+	
+	
 });
